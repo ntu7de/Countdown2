e9d4317abb8b81aae0f980ea9fb930a5b44299a8
Patrick Yuson <pyuson@Patricks-MacBook-Pro.local>
2024-04-16 17:13:31 -0400

    Added buttons to hide books

--- bookstore-app/src/components/BookList.jsx
+++ bookstore-app/src/components/BookList.jsx
@@ -1,14 +1,14 @@
 
 export const BookList = ({ arrOfBooks }) => {
 
-    console.log(arrOfBooks);
+    // console.log(arrOfBooks);
     // console.log("Printed.");
 
     return (
         <>
             {arrOfBooks.map((book) => { return (
                 <ul>
-                    <li>{book.title}, {book.author}, {book.price}</li>
+                    <li key={book.title}>{book.title}, {book.author}, {book.price}</li>
                 </ul>
             )})}
         </>
--- bookstore-app/src/components/BookStore.jsx
+++ bookstore-app/src/components/BookStore.jsx
@@ -1,18 +1,35 @@
 import { BookList } from "./BookList";
+import { useState } from "react";
 
 export const BookStore = ( bookData ) => {
 
     const arrOfGenres = Object.keys(bookData['bookData']);
+    
+    // Add state to keep track of genres selected
+    const [selectedGenres, setSelectedGenres] = useState(arrOfGenres);
 
     // console.log(bookData['bookData']['children']);
     // console.log(arrOfGenres);
     // console.log("Hello???");
+
+    function selectGenres(genreToHide) {
+        const indexToRemove = selectedGenres.indexOf(genreToHide);
+        // Can't edit selectedGenres
+        let newGenres = selectedGenres.slice();
+        if (indexToRemove !== -1) {
+            newGenres.splice(indexToRemove, 1);
+        }
+        setSelectedGenres(newGenres);
+    }
     
     return (
         <>
-            {arrOfGenres.map((genre) => { return (
+            <button onClick={() => selectGenres('fiction')}>Hide Fiction</button>
+            <button onClick={() => selectGenres('non-fiction')}>Hide Non-Fiction</button>
+            <button onClick={() => selectGenres('children')}>Hide Children</button>
+            {selectedGenres.map((genre) => { return (
                 <ul key={genre}>
-                    <li><h2>{genre}</h2></li>
+                    <li><h2>{genre}:</h2></li>
                     <BookList arrOfBooks={bookData['bookData'][genre]}/>
                 </ul>
             )})}
