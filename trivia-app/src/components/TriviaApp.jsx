902793a2bf219a17111a834aeb17aeb86a643791
Patrick Yuson <pyuson@Patricks-MBP.home>
2024-04-19 19:09:41 -0400

    got the questions to display

--- trivia-app/src/components/TriviaApp.jsx
+++ trivia-app/src/components/TriviaApp.jsx
@@ -1,13 +1,30 @@
-import React, { useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TriviaApp = () => {
 
+    const [questions, setQuestions] = useState([]);
+    
     useEffect(() => {
         // fetch trivia questions
+        const fetchData = async() => {
+            const data = await fetch("https://the-trivia-api.com/v2/questions");
+            const json = await data.json();
+            setQuestions(json);
+            console.log(json[0]['question']['text']);
+        }
+
+        fetchData();
+
     }, [])
 
     return (
-        <h1>Trivia App</h1>
+        <>
+            <h1>Trivia App</h1>
+            <p></p>
+            {questions.map((questionData, index) => { return (
+                <li key={index}>{questionData.question.text}</li>
+            )})}
+        </>
     )
 }
 
